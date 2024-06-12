775aa280e32f5145b17bd0c8a90147d8b0b99cd6
Edins <eddinsstivons@gmail.com>
2024-06-12 13:55:18 +0300

    made some adjustment

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 import 'package:njeve/resources/color.dart';
 
 class HomePage extends StatefulWidget {
@@ -27,7 +29,19 @@ class _HomePageState extends State<HomePage> {
                     colors: [topColor, ColorList.backgroundColor])),
           ),
           Column(
-            children: [topBar()],
+            children: [
+              topBar(),
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    iconLottie(),
+                    feelsLikeTemp(),
+                    otherReads()
+                  ],
+                ),
+              ),
+            ],
           )
         ],
       ),
@@ -42,14 +56,13 @@ class _HomePageState extends State<HomePage> {
       padding: EdgeInsets.only(
           left: 10,
           right: 10,
-          top: MediaQuery.of(context).viewPadding.top > 0 ? 35 : 5,
-          bottom: 5),
+          top: MediaQuery.of(context).viewPadding.top > 0 ? 35 : 5),
       width: double.infinity,
       child: Row(
         children: [
           Expanded(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Row(
@@ -62,7 +75,9 @@ class _HomePageState extends State<HomePage> {
                         color: ColorList.backgroundColor,
                         fontSize: 20),
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Icon(
                     Icons.arrow_outward_rounded,
                     color: ColorList.backgroundColor,
@@ -94,4 +109,102 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  Widget iconLottie() {
+    return Lottie.asset("lottie/sun.json", width: 350, height: 350);
+  }
+
+
+  Widget feelsLikeTemp(){
+    return const Padding(
+      padding: EdgeInsets.only(left: 10, right: 10),
+      child: Row(
+        children: [
+          Text(
+            "62.3°C",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: ColorList.textColor,
+                fontSize: 60),
+          ),
+          SizedBox(width: 5,),
+          Expanded(child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Rain, Partially cloudy",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    color: ColorList.textColor,
+                    fontSize: 12),
+              ),
+              Text(
+                "Feels Like 60.3°",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    color: ColorList.textColor,
+                    fontSize: 10),
+              ),
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+
+  Widget textItems({required String key,required String value}){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          key,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              color: ColorList.textColor,
+              fontSize: 11),
+        ),
+        Text(
+          value,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              color: ColorList.textColor,
+              fontSize: 11),
+        )
+      ],
+    );
+  }
+
+  Widget otherReads(){
+    return Padding(
+      padding: const EdgeInsets.only(left: 10,right: 10),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                textItems(key: 'Humidity', value: '18%'),
+                textItems(key: 'Wind', value: '12km/hr'),
+              ],
+            ),
+          ),
+
+          Expanded(
+            child: Column(
+              children: [
+                textItems(key: 'Air Quality', value: '63'),
+                textItems(key: 'Visibility', value: '13km'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
